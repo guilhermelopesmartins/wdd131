e260e10af94173eadbf6648da1f7f5ce6cb7b7d0
guilhermelopesmartins <guirockmartins@hotmail.com>
2024-12-02 22:25:20 +0100

    login and media for dialog

---
+++ project/scripts/login.js
@@ -0,0 +1,9 @@
+document.getElementById('login-btn').addEventListener('click', () => {
+    const username = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    if (username != 'test' && password != '123') {
+        alert('Name or Password wrong, try again.');
+        return;
+    }
+    window.location.replace("home.html");
+});
